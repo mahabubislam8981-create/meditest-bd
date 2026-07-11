420f7c0a0b51e0b7506c158b9d3ed8b81f0acee2
mahabubislam8981-create <mahabub.islam8981@gmail.com>
2026-07-11 21:51:14 +0600

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,43 @@
+import { db } from "./firebase.js";
+import {
+  collection,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
+// URL থেকে টেস্টের নাম নেওয়া
+const params = new URLSearchParams(window.location.search);
+const testName = params.get("name");
+
+// HTML Elements
+const name = document.getElementById("name");
+const category = document.getElementById("category");
+const price = document.getElementById("price");
+const description = document.getElementById("description");
+const sample = document.getElementById("sample");
+const preparation = document.getElementById("preparation");
+const reportTime = document.getElementById("reportTime");
+
+// Firestore থেকে ডাটা আনা
+async function loadTest() {
+  const snapshot = await getDocs(collection(db, "tests"));
+
+  snapshot.forEach((doc) => {
+
+    const test = doc.data();
+
+    if (test.name === testName) {
+
+      name.textContent = test.name;
+      category.textContent = test.category || "তথ্য নেই";
+      price.textContent = test.price || "";
+      description.textContent = test.description || "তথ্য নেই";
+      sample.textContent = test.sample || "তথ্য নেই";
+      preparation.textContent = test.preparation || "তথ্য নেই";
+      reportTime.textContent = test.reportTime || "তথ্য নেই";
+
+    }
+
+  });
+}
+
+loadTest();
