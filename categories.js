3bde6799e44366a44dfb6dc96cf6130e59b75bd8
mahabubislam8981-create <mahabub.islam8981@gmail.com>
2026-07-12 20:19:55 +0600

    Update categories.js

--- categories.js
+++ categories.js
@@ -1,1 +1,43 @@
+import { db } from "./firebase.js";
+import { collection, getDocs } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
+const categoryList = document.getElementById("categoryList");
+
+async function loadCategories() {
+  categoryList.innerHTML = "<p>লোড হচ্ছে...</p>";
+
+  try {
+    const snapshot = await getDocs(collection(db, "categories"));
+
+    if (snapshot.empty) {
+      categoryList.innerHTML = "<p>কোনো ক্যাটাগরি পাওয়া যায়নি।</p>";
+      return;
+    }
+
+    categoryList.innerHTML = "";
+
+    snapshot.forEach((doc) => {
+      const category = doc.data();
+
+      categoryList.innerHTML += `
+        <div class="card">
+          <h2>${category.icon || "📂"} ${category.name}</h2>
+
+          <p>${category.description || ""}</p>
+
+          <a href="index.html?category=${encodeURIComponent(category.name)}">
+            <button class="details-btn">
+              এই ক্যাটাগরির টেস্ট দেখুন
+            </button>
+          </a>
+        </div>
+      `;
+    });
+
+  } catch (error) {
+    console.error(error);
+    categoryList.innerHTML = "<p>ক্যাটাগরি লোড করা যায়নি।</p>";
+  }
+}
+
+loadCategories();
