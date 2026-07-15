b1fead9f34e1c6e4423d1b2bf3f42a99fa25e6f0
mahabubislam8981-create <mahabub.islam8981@gmail.com>
2026-07-15 23:04:25 +0600

    Update doctor-details.js

--- doctor-details.js
+++ doctor-details.js
@@ -1,1 +1,80 @@
+import { db } from "./firebase.js";
 
+import {
+collection,
+getDocs
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+const params = new URLSearchParams(window.location.search);
+const slug = params.get("slug");
+
+const disease = document.getElementById("disease");
+const department = document.getElementById("department");
+const doctor = document.getElementById("doctor");
+const symptoms = document.getElementById("symptoms");
+const description = document.getElementById("description");
+const recommendedTests = document.getElementById("recommendedTests");
+const note = document.getElementById("note");
+const emergency = document.getElementById("emergency");
+
+async function loadDoctor(){
+
+try{
+
+const snapshot = await getDocs(collection(db,"doctor_guide"));
+
+let found=false;
+
+snapshot.forEach(doc=>{
+
+const data=doc.data();
+
+if(data.slug===slug){
+
+found=true;
+
+disease.textContent=data.disease || "-";
+department.textContent=data.department || "-";
+doctor.textContent=data.doctor || "-";
+symptoms.textContent=data.symptoms || "-";
+description.textContent=data.description || "-";
+note.textContent=data.note || "-";
+
+emergency.textContent=
+data.emergency ? "হ্যাঁ" : "না";
+
+recommendedTests.innerHTML="";
+
+if(Array.isArray(data.recommendedTests)){
+
+data.recommendedTests.forEach(test=>{
+
+recommendedTests.innerHTML += `
+<p>🧪 ${test}</p>
+`;
+
+});
+
+}
+
+}
+
+});
+
+if(!found){
+
+disease.textContent="তথ্য পাওয়া যায়নি";
+
+}
+
+}
+
+catch(error){
+
+console.error(error);
+
+}
+
+}
+
+loadDoctor();
